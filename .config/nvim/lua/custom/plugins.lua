c0efc904e7c430a5c05631b12e93784be53db745
Robin Persson <robin.persson@glimworks.com>
2024-01-26 08:50:57 +0100

    .config/nvim/lua/custom/plugins.lua: Added iamcco/markdown-preview.nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,81 +1,93 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "vue",
-        "svelte",
-      },
-      automatic_installation = true,
-    },
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "yaml-language-server",
-        "dockerfile-language-server",
-        "html-lsp",
-        "json-lsp",
-        "stylua",
-        "eslint_d",
-        "jsonlint",
-        "typescript-language-server",
-      },
-    },
-  },
-  {
-    "github/copilot.vim",
-    lazy = false,
-    config = function()
-      vim.g.copilot_assume_mapped = true
-    end,
-  },
-  {
-    "tpope/vim-dadbod",
-    lazy = false,
-  },
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "javascript", "typescript", "sql", "mongodb", "mysql", "plsql" }, lazy = true },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"vue",
+				"svelte",
+			},
+			automatic_installation = true,
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require "custom.configs.null-ls"
+			end,
+		},
+		config = function()
+			require "plugins.configs.lspconfig"
+			require "custom.configs.lspconfig"
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"yaml-language-server",
+				"dockerfile-language-server",
+				"html-lsp",
+				"json-lsp",
+				"stylua",
+				"eslint_d",
+				"jsonlint",
+				"typescript-language-server",
+			},
+		},
+	},
+	{
+		"github/copilot.vim",
+		lazy = false,
+		config = function()
+			vim.g.copilot_assume_mapped = true
+		end,
+	},
+	{
+		"tpope/vim-dadbod",
+		lazy = false,
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{
+				"kristijanhusak/vim-dadbod-completion",
+				ft = { "javascript", "typescript", "sql", "mongodb", "mysql", "plsql" },
+				lazy = true,
+			},
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
 return plugins
