34ade784d502fbb12732c91d05438d8619aa0b29
Robin Persson <robin.persson@glimworks.com>
2024-01-30 20:04:46 +0100

    .wezterm.lua: Added

---
+++ .wezterm.lua
@@ -0,0 +1,18 @@
+
+local wezterm = require 'wezterm'
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.dpi = 192
+config.color_scheme = 'Night Owl (Gogh)'
+config.font = wezterm.font('JetBrainsMono Nerd Font', {weight='Bold'})
+config.font_size = 12
+config.use_dead_keys = false
+config.scrollback_lines = 5000
+config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
